ce313d83c4ef7d9bb380f5c61a2a9f216174eee5
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-08-30 13:38:56 +0900

    Implement rightmost function to return the rightmost key from a given string

--- src/rightmost.ts
+++ src/rightmost.ts
@@ -1,3 +1,9 @@
 export function rightmost(keys: string): string {
+    const keyOrder = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    for (let i = keyOrder.length - 1; i >= 0; i--) {
+        if (keys.includes(keyOrder[i])) {
+            return keyOrder[i]
+        }
+    }
     return ''
 }
