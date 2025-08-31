516c89f4a7987aa9a926b082b2832c6d2f82029f
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-08-31 20:58:14 +0900

    wip

--- src/number2kanji.ts
+++ src/number2kanji.ts
@@ -1,3 +1,21 @@
-export function number2kanji(x: number): number {
-    return 0
+export function number2kanji(x: number): string {
+    // 5678912345 という数字を [56,7891,2345] のように4桁ずつ区切る
+
+function singleDigitToKanji(d: number): string {
+    const kanjiDigits = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+    return kanjiDigits[d]
+}
+
+function digitToKanji(d: number): string {
+    const kanjiDigits = [
+        '', '十', '百', '千', 
+    ]
+    return kanjiDigits[d]
+}
+
+function largeDigitToKanji(d: number): string {
+    const kanjiDigits = [
+        '', '万', '億', '兆', '京'
+    ]
+    return kanjiDigits[d]
 }
