4438582e6ff02f31687ae23205fd2ef2a365c2b6
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-09-01 22:30:24 +0900

    evalexpr1関数を実装し、数式をトークンに分割して計算する処理を追加

--- src/evalexpr1.ts
+++ src/evalexpr1.ts
@@ -1,3 +1,38 @@
 export function evalexpr1(expr: string): number {
-    return 0
+    // '1+1' を [1, '+', 1] に分割する
+    // '12+345' を [12, '+', 345] に分割する
+    const tokens = expr.split(/(\+|\-|\*)/).map(token => {
+        const n = Number(token)
+        return isNaN(n) ? token : n
+    })
+
+    const stack: (number | string)[] = []
 }
+
+function calculate(operator: string): ((left: number, right: number) => number) {
+    switch (operator) {
+        case '+': return plus
+        case '-': return minus
+        case '*': return multiply
+        case '/': return devide
+        default: throw new Error(`不明な演算子: ${operator}`)
+    }
+}
+
+function plus(left: number, right: number): number {
+    return left + right
+}
+
+function minus(left: number, right: number): number {
+    return left - right
+}
+
+function multiply(left: number, right: number): number {
+    return left * right
+}
+
+function devide(left: number, right: number): number {
+    return left / right
+}
+
+
